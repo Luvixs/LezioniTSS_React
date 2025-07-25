72902924d19266114c30503b9fefbb5921a2e7b4
Luvixs <148796042+Luvixs@users.noreply.github.com>
2025-07-25 13:53:44 +0200

    Add files via upload

---
+++ appReact01/src/component/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+
+    return (
+        <>
+        <ul>
+            <li>pg 1</li>
+            <li>pg 2</li>
+            <li>pg 3</li>
+        </ul>
+        </>
+    )
+}
+
+export default Navbar
